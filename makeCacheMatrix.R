3c12617622b024bb406de6b822fddaf1a86c3d63
gsumant97 <sumant.gupta@somaiya.edu>
2020-07-23 15:37:54 +0530

    Week-3 Peer graded assessment
    makeCacheMatrix

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,3 +1,5 @@
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()){
   inv <- NULL
   set <- function(y){
@@ -11,7 +13,11 @@ makeCacheMatrix <- function(x = matrix()){
 
 }
 
+
+## Write a short comment describing this function
 cachesolve <- function(x, ...){
+  ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
   if(!is.null(inv)){
     message("getting cached data")
